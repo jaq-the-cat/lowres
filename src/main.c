bef966409b8ec2dc88ddfbb2e989f192227bc33a
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 11:39:14 -0300

    this is so awesome lol

--- src/main.c
+++ src/main.c
@@ -14,22 +14,23 @@ int main() {
     curs_set(0);
 
     Point cam = P(0, 0, 1);
-    int w = 800, h = 300;
-    Point p0  = P(-w, -h, 10);
-    Point p1  = P( w, -h, 10);
-    Point p2  = P( w,  h, 10);
-    Point p3  = P(-w,  h, 10);
+    int w = 1600, h = 800;
+    int xoff = 0, yoff = 0;
+    Point p0  = P(-w/2 + xoff, -h/2 + yoff, 10);
+    Point p1  = P( w/2 + xoff, -h/2 + yoff, 10);
+    Point p2  = P( w/2 + xoff,  h/2 + yoff, 10);
+    Point p3  = P(-w/2 + xoff,  h/2 + yoff, 10);
 
-    for (int i=0; i<100; i++) {
+    while (1) {
         clear();
         draw_3d_line(p0, p1, cam.z);
         draw_3d_line(p1, p2, cam.z);
         draw_3d_line(p2, p3, cam.z);
         draw_3d_line(p3, p0, cam.z);
+        p0.z -= 1;
         p1.z -= 1;
-        p2.z -= 1;
         refresh();
-        napms(30);
+        getch();
     }
 
     curs_set(1);
