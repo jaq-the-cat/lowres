5875cc91da82ea4351dc88d31e956bd4b7c280e0
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 09:57:34 -0300

    display_line

--- src/render.c
+++ src/render.c
@@ -3,3 +3,13 @@
 int trans_c(float c, float max_world_c, float max_term_c) {
     return c / (max_world_c / max_term_c);
 }
+
+void display_line(int y0, int x0, int y1, int x1) {
+    int maxy, maxx;
+    getmaxyx(stdscr, maxy,  maxx);
+
+    y0 = trans_c(y0, WORLD_HEIGHT, maxy);
+    x0 = trans_c(x0, WORLD_WIDTH, maxx);
+    y1 = trans_c(y1, WORLD_HEIGHT, maxy);
+    x1 = trans_c(x1, WORLD_WIDTH, maxx);
+}
--- src/render.h
+++ src/render.h
@@ -3,9 +3,10 @@
 
 #include <ncurses.h>
 
-#define WORLD_WIDTH 500
 #define WORLD_HEIGHT 200
+#define WORLD_WIDTH 500
 
-int trans_c(float, float, float);
+int trans_c(float c, float max_world_c, float max_term_c);
+void display_line(int y0, int x0, int y1, int x1);
 
 #endif
