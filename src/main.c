26625c30cd6738aebdfa3878e95016152c8c6d3f
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 11:06:15 -0300

    walll

--- src/main.c
+++ src/main.c
@@ -10,17 +10,23 @@ void print_n(int y, int x, float n) {
 }
 
 int main() {
+    initscr();
+    curs_set(0);
 
     Point camera = P(0, 0, 0);
     float d = 10;
-    Point p0 = P(-20, 0, 200);
-    Point p1 = P(20, 0, 200);
-    initscr();
-    curs_set(0);
+    Point p0 = P(-20, -10, 200);
+    Point p1 = P( 20, -10, 200);
+    Point p2 = P( 20,  10, 200);
+    Point p3 = P(-20,  10, 200);
+
     for (int i=0; i<100; i++) {
         d+=2;
         clear();
         draw_3d_line(p0, p1, d);
+        draw_3d_line(p1, p2, d);
+        draw_3d_line(p2, p3, d);
+        draw_3d_line(p3, p0, d);
         refresh();
         napms(30);
     }
