ac46df8c3a05ea76eab588935ff4e31415e15b6a
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 15:22:24 -0300

    :P bad

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "3d.h"
 #include "render.h"
 
 void print_n(int y, int x, float n) {
@@ -13,19 +12,9 @@ int main() {
     initscr();
     curs_set(0);
 
-    Point cam = P(0, 0, 1);
-    int w = 5000, h = 1600;
-    int xoff = 0, yoff = 0;
-    Point p0  = P(-w/2 + xoff, -h/2 + yoff, 100);
-    Point p1  = P( w/2 + xoff, -h/2 + yoff, 100);
-    Point p2  = P( w/2 + xoff,  h/2 + yoff, 100);
-    Point p3  = P(-w/2 + xoff,  h/2 + yoff, 100);
 
     while (1) {
         clear();
-        draw_quad(p0, p1, p2, p3, cam);
-        p0.z += -2;
-        p1.z += -2;
         refresh();
         getch();
     }
