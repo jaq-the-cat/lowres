a5a32354c6499011ad724162cb9ea3aafafd7a9a
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 10:34:17 -0300

    aa

--- src/main.c
+++ src/main.c
@@ -14,11 +14,6 @@ int main() {
     Point p0 = P(200, 100, 20);
     Point p1 = P(300, 100, 20);
 
-    int sy0 = to_screen(p0.y, p0.z, s);
-    int sx0 = to_screen(p0.x, p0.z, s);
-    int sy1 = to_screen(p1.y, p1.z, s);
-    int sx1 = to_screen(p1.x, p1.z, s);
-
     initscr();
 
     for (int i=0; i<WORLD_HEIGHT; i++) {
