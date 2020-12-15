95941c56362ce71c98bb8c0176ce692c2aee81f2
jaq-cat <jaq.cat@protonmail.ch>
2020-12-15 10:34:31 -0300

    ree

--- src/main.c
+++ src/main.c
@@ -11,14 +11,11 @@ int main() {
 
     Point camera = P(0, 0, 0);
     Point s = P(0, 0, 5);
-    Point p0 = P(200, 100, 20);
-    Point p1 = P(300, 100, 20);
 
     initscr();
 
     for (int i=0; i<WORLD_HEIGHT; i++) {
         clear();
-        display_line(sy0, sx0, sy1, sx1);
         refresh();
         napms(50);
     }
