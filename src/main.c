3153a233c97dd6b082f244070a5c45ab50bdd639
jaq-cat <jaq.cat@protonmail.ch>
2020-12-16 17:29:39 -0300

    fucking yEET

--- src/main.c
+++ src/main.c
@@ -11,22 +11,29 @@ void print_n(int y, int x, float n) {
     mvaddstr(y, x, buff);
 }
 
+void rotate_point(Point c, Point *p, double angle) {
+    p->x = cos(angle) * (p->x - c.x) - sin(angle) * (p->y - c.y) + c.x;
+    p->y = sin(angle) * (p->x - c.x) + cos(angle) * (p->y - c.y) + c.y;
+}
+
+void rotate_line(Point c, Line *l, double angle) {
+    rotate_point(c, &l->p0, angle);
+    rotate_point(c, &l->p1, angle);
+}
+
 int main() {
     initscr();
     curs_set(0);
     
     Player p = PLAYER(WORLD_WIDTH/2, WORLD_HEIGHT/2, 0);
-    Line wall = LINE(
-            WORLD_WIDTH/2 - 10, WORLD_HEIGHT/2 - 40,
-            WORLD_WIDTH/2 + 10, WORLD_HEIGHT/2 - 10
-            );
-
+    Point o = POINT(WORLD_WIDTH/2 - 10, WORLD_HEIGHT/2 - 40);
     while (1) {
         clear();
-        display_line(wall.p0.y, wall.p0.x, wall.p1.y, wall.p1.x);
+        display_point(o.y, o.x, 'C');
         print_player(&p);
         refresh();
         getch();
+        rotate_point(p.origin, &o, 0.2);
     }
 
     curs_set(1);
