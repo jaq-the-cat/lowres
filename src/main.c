6dc084f5dfec5d25b1a58adbb6a8a4971b279c93
jaq-cat <jaq.cat@protonmail.ch>
2020-12-16 21:03:30 -0300

    movement

--- src/main.c
+++ src/main.c
@@ -12,16 +12,31 @@ int main() {
     curs_set(0);
     
     Player p = PLAYER(0, 0, 0);
+    double movement_vec[2] = {0};
+
     Line wall = {
         POINT(-20, -40),
         POINT( 20, -40),
     };
+
     while (1) {
         clear();
         display_line(wall[0].y, wall[0].x, wall[1].y, wall[1].x);
         print_player(&p);
         refresh();
         switch (getch()) {
+            case 'w':
+                movement_vec[1] = 5;
+                break;
+            case 'q':
+                movement_vec[0] = -5;
+                break;
+            case 's':
+                movement_vec[1] = -5;
+                break;
+            case 'e':
+                movement_vec[0] = 5;
+                break;
             case 'd':
                 rotate_line(p.origin, wall, RAD(-ROTATION));
                 break;
