8c2a5c8c9b5b0fd82e2863f47f8595d2b9d5fda7
jaq-cat <jaq.cat@protonmail.ch>
2020-12-16 21:01:44 -0300

    rotations with keys

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "player.h"
 #include "3d.h"
 
+#define ROTATION 12
+
 int main() {
     initscr();
     curs_set(0);
@@ -19,8 +21,14 @@ int main() {
         display_line(wall[0].y, wall[0].x, wall[1].y, wall[1].x);
         print_player(&p);
         refresh();
-        getch();
-        rotate_line(p.origin, wall, RAD(12));
+        switch (getch()) {
+            case 'd':
+                rotate_line(p.origin, wall, RAD(-ROTATION));
+                break;
+            case 'a':
+                rotate_line(p.origin, wall, RAD(ROTATION));
+                break;
+        }
     }
 
     curs_set(1);
