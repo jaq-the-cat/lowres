5019927fc4f0a26851a575cf77936efbfe78ac28
jaq-cat <jaq.cat@protonmail.ch>
2020-12-16 21:33:23 -0300

    fuck

--- src/3d.c
+++ src/3d.c
@@ -76,8 +76,9 @@ void display_line(int y0, int x0, int y1, int x1) {
 void display_wall(Line wall) {
     display_line(
         wall[0].x / wall[0].y,
-        0 / wall[0].y,
+        1 / wall[0].y,
+
         wall[1].x / wall[1].y,
-        0 / wall[1].y
+        1 / wall[1].y
     );
 }
